bd0b72783494f86d832d2ec36d672ad2b456422f
Bobrov Evgeniy <mbober2112@gmail.com>
2021-12-06 13:02:57 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/State';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, changeNewMessage, changeNewPost, sendMessage } from './redux/State';
+import store from './redux/State';
 
 export let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
             <App
                 state={state}
-                addPost={addPost}
-                changeNewPost={changeNewPost} 
-                sendMessage={sendMessage}
-                changeNewMessage={changeNewMessage}/>
+                addPost={store.addPost.bind(store)}
+                changeNewPost={store.changeNewPost.bind(store)} 
+                sendMessage={store.sendMessage.bind(store)}
+                changeNewMessage={store.changeNewMessage.bind(store)}/>
         </BrowserRouter>, document.getElementById('root')
     );
 }
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,96 +1,101 @@
-
-let rerenderEntireTree = () => {
-}
-
-let state = {
-    profilePage: {
-        myPostsData: [
-            { id: 1, title: "Пост 1", text: "Текст", likes: 12, dislikes: 3 },
-            { id: 2, title: "Пост 2", text: "Текст", likes: 16, dislikes: 5 },
-            { id: 3, title: "Пост 3", text: "Текст", likes: 21, dislikes: 8 },
-            { id: 4, title: "Пост 4", text: "Текст", likes: 16, dislikes: 2 },
-        ],
-        newPostTitle: "",
-        newPostText: "",
+let store = {
+    _state: {
+        profilePage: {
+            myPostsData: [
+                { id: 1, title: "Пост 1", text: "Текст", likes: 12, dislikes: 3 },
+                { id: 2, title: "Пост 2", text: "Текст", likes: 16, dislikes: 5 },
+                { id: 3, title: "Пост 3", text: "Текст", likes: 21, dislikes: 8 },
+                { id: 4, title: "Пост 4", text: "Текст", likes: 16, dislikes: 2 },
+            ],
+            newPostTitle: "",
+            newPostText: "",
+        },
+        dialogsPage: {
+            dialogsData: [
+                { id: 1, name: "Pavel" },
+                { id: 2, name: "Petr" },
+                { id: 3, name: "Dima" },
+                { id: 4, name: "Anton" },
+            ],
+            messagesData: [
+                { id: 1, message: "Hello" },
+                { id: 2, message: "Hi" },
+                { id: 3, message: "How are you?" },
+                { id: 4, message: "Bye" },
+            ],
+            newMessageText: "",
+        },
+        allPostsPage: {
+            allPostsData: [
+                { id: 1, username: "Pavel", title: "Заголовок 1", text: "Текст", likes: 12, dislikes: 3, saved: true },
+                { id: 2, username: "Petr", title: "Заголовок 2", text: "Текст", likes: 16, dislikes: 5, saved: false },
+                { id: 3, username: "Dima", title: "Заголовок 3", text: "Текст", likes: 21, dislikes: 8, saved: false },
+                { id: 4, username: "Anton", title: "Заголовок 4", text: "Текст", likes: 16, dislikes: 2, saved: true },
+            ],
+        }, 
     },
-    dialogsPage: {
-        dialogsData: [
-            { id: 1, name: "Pavel" },
-            { id: 2, name: "Petr" },
-            { id: 3, name: "Dima" },
-            { id: 4, name: "Anton" },
-        ],
-        messagesData: [
-            { id: 1, message: "Hello" },
-            { id: 2, message: "Hi" },
-            { id: 3, message: "How are you?" },
-            { id: 4, message: "Bye" },
-        ],
-        newMessageText: "",
+
+    getState() {
+        return this._state;
     },
-    allPostsPage: {
-        allPostsData: [
-            { id: 1, username: "Pavel", title: "Заголовок 1", text: "Текст", likes: 12, dislikes: 3, saved: true },
-            { id: 2, username: "Petr", title: "Заголовок 2", text: "Текст", likes: 16, dislikes: 5, saved: false },
-            { id: 3, username: "Dima", title: "Заголовок 3", text: "Текст", likes: 21, dislikes: 8, saved: false },
-            { id: 4, username: "Anton", title: "Заголовок 4", text: "Текст", likes: 16, dislikes: 2, saved: true },
-        ],
-    }, 
+
+    _callSubscriber() {
+    },
+
+    addPost() {
+        let newPost = {
+            id: 5,
+            title: this._state.profilePage.newPostTitle,
+            text: this._state.profilePage.newPostText,
+            likes: 0,
+            dislikes: 0
+        }
+    
+        let newAllPost ={
+            id: 5,
+            username: "Username",
+            title: this._state.profilePage.newPostTitle,
+            text: this._state.profilePage.newPostText,
+            likes: 0,
+            dislikes: 0,
+            saved: false
+        }
+    
+        this._state.profilePage.myPostsData.push(newPost);
+        this._state.allPostsPage.allPostsData.push(newAllPost);
+    
+        this._state.profilePage.newPostTitle = "";
+        this._state.profilePage.newPostText = "";
+    
+        this._callSubscriber(this._state);
+    },
+
+    changeNewPost(newTitle, newText) {
+        this._state.profilePage.newPostTitle = newTitle;
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    sendMessage() {
+        let newMessage = {
+            id: 5,
+            message: this._state.dialogsPage.newMessageText,
+        }
+    
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._state.dialogsPage.newMessageText = "";
+    
+        this._callSubscriber(this._state);
+    },
+
+    changeNewMessage(newMessage) {
+        this._state.dialogsPage.newMessageText = newMessage;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        title: state.profilePage.newPostTitle,
-        text: state.profilePage.newPostText,
-        likes: 0,
-        dislikes: 0
-    }
-
-    let newAllPost ={
-        id: 5,
-        username: "Username",
-        title: state.profilePage.newPostTitle,
-        text: state.profilePage.newPostText,
-        likes: 0,
-        dislikes: 0,
-        saved: false
-    }
-
-    state.profilePage.myPostsData.push(newPost);
-    state.allPostsPage.allPostsData.push(newAllPost);
-
-    state.profilePage.newPostTitle = "";
-    state.profilePage.newPostText = "";
-
-    rerenderEntireTree(state);
-}
-
-export const changeNewPost = (newTitle, newText) => {
-    state.profilePage.newPostTitle = newTitle;
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const sendMessage = () => {
-    let newMessage = {
-        id: 5,
-        message: state.dialogsPage.newMessageText,
-    }
-
-    state.dialogsPage.messagesData.push(newMessage);
-    state.dialogsPage.newMessageText = "";
-
-    rerenderEntireTree(state);
-}
-
-export const changeNewMessage = (newMessage) => {
-    state.dialogsPage.newMessageText = newMessage;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
